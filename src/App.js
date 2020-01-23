2ce2456e5da86986e050be762098b08d79b87325
logeshdeveloper <logeshdeveloper8@gmail.com>
2020-01-23 23:57:47 +0530

    Invoking Ajax

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Translator extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      items: []
+    }
+  }
+
+  componentDidMount(){
+    fetch("http://146.148.85.67/processWordJSON?inString&lang")
+    .then(res => res.json())
+    .then((data) =>{
+      this.setState({items: data.twords})
+    })
+
+  }
+
+  render(){
+    return(
+      <div>
+        Enter the text:
+      <input type="text" name="text"/>
+      Translated text:
+      <input type="text" name="text"/>
+      <input list="hosting-plan" type="text"/>
+    <datalist id="hosting-plan">
+     <option value="small"/>
+     <option value="medium"/>
+     <option value="large"/>
+    </datalist>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Translator; 
