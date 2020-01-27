af3d1d9006b546c0b7d3d02d55f194fed10f13ea
logeshdeveloper <logeshdeveloper8@gmail.com>
2020-01-27 07:45:59 +0530

    Errors Fixed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,62 @@ import './App.css';
 
 
 function App() {
-  
+    window.onload = function(){
+    const addElement = text => {
+        // create a new div element
+        var newDiv = document.createElement("div");
+        // and give it some content
+        var newContent = document.createTextNode(text);
+        // add the text node to the newly created div
+        newDiv.appendChild(newContent);
+    
+        // add the newly created element and its content into the DOM
+        suggestionContainer.appendChild(newDiv);
+    };
+
+    const removeElement = () => suggestionContainer.innerHTML = '';
+    
+    let timeout = null;
+    let contentArea = document.querySelector('#description_textarea');
+    let languageSelection = document.querySelector('#language_dropdown');
+    let suggestionContainer = document.querySelector('.description_suggestion');
+    
+    contentArea.addEventListener('keyup', (e) => {
+      const _elem = e.target || e.currentTarget;
+      // PICK THE LAST WORD
+      const clearArr = _elem.value.split(' ').filter(function(el) { return el; })
+      const _val = clearArr[clearArr.length - 1];
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+          removeElement();
+          const params = `?inString=${_val}&lang=${getTextAreaValue()}`;
+          ajax_xhr(params).then(res => {
+              const opts = res.twords[0].options;
+              if (opts.length) {
+                  for(var i = 0; i < opts.length; i++) {
+                      addElement(opts[i]);
+                  }
+              }
+          }, err => console.log(err));
+      }, 1500);
+    });
+
+    
+    // GET TEXTAREA VALUE
+    const getTextAreaValue = () => languageSelection.options[languageSelection.selectedIndex].value.toLowerCase();
+
+    async function ajax_xhr(str = '') {
+        if (str === '') return;
+        let response = await fetch(`http://146.148.85.67/processWordJSON${str}`,  { method: 'GET' })
+        let data = await response.json();
+        return data;
+    };
+}
   return (
     <div className="main_container">
         <fieldset>
             <legand>
-                <label for="language_dropdown">Language:</label>
+                <label>Language:</label>
                 <span className="language_dropdown_span">
                     <select id="language_dropdown" className="language_dropdown">
                         <option>Tamil</option>
@@ -24,58 +74,6 @@ function App() {
         </fieldset>
     </div>
   );
-
-  const addElement = text => {
-    // create a new div element
-    var newDiv = document.createElement("div");
-    // and give it some content
-    var newContent = document.createTextNode(text);
-    // add the text node to the newly created div
-    newDiv.appendChild(newContent);
-
-    // add the newly created element and its content into the DOM
-    suggestionContainer.appendChild(newDiv);
-};
- 
-
-const removeElement = () => suggestionContainer.innerHTML = '';
-
-let timeout = null;
-let contentArea = document.querySelector('#description_textarea');
-let languageSelection = document.querySelector('#language_dropdown');
-let suggestionContainer = document.querySelector('.description_suggestion');
-
-contentArea.addEventListener('keyup', (e) => {
-  const _elem = e.target || e.currentTarget;
-  // PICK THE LAST WORD
-  const clearArr = _elem.value.split(' ').filter(function(el) { return el; })
-  const _val = clearArr[clearArr.length - 1];
-  clearTimeout(timeout);
-  timeout = setTimeout(() => {
-      removeElement();
-      const params = `?inString=${_val}&lang=${getTextAreaValue()}`;
-      ajax_xhr(params).then(res => {
-          const opts = res.twords[0].options;
-          if (opts.length) {
-              for(var i = 0; i < opts.length; i++) {
-                  addElement(opts[i]);
-              }
-          }
-      }, err => console.log(err));
-  }, 1500);
-});
-
-
-// GET TEXTAREA VALUE
-const getTextAreaValue = () => languageSelection.options[languageSelection.selectedIndex].value.toLowerCase();
-  
-async function ajax_xhr(str = '') {
-    if (str === '') return;
-    let response = await fetch(`http://146.148.85.67/processWordJSON${str}`,  { method: 'GET' })
-    let data = await response.json();
-    return data;
-};
-
 
 }
 
