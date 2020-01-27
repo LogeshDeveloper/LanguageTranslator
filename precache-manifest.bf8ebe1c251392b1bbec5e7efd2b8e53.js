73c971fbb52259b2024270ac866073c48f236a88
logeshdeveloper <logeshdeveloper8@gmail.com>
2020-01-27 19:01:02 +0530

    Updates

---
+++ precache-manifest.bf8ebe1c251392b1bbec5e7efd2b8e53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad90d94507759d32a0791b7d945cf454",
+    "url": "/LanguageTranslator/index.html"
+  },
+  {
+    "revision": "f5add0f82b12e2bdcb78",
+    "url": "/LanguageTranslator/static/css/main.a076f78a.chunk.css"
+  },
+  {
+    "revision": "2b7bfa0cc936c42330a9",
+    "url": "/LanguageTranslator/static/js/2.1eb297e8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/LanguageTranslator/static/js/2.1eb297e8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5add0f82b12e2bdcb78",
+    "url": "/LanguageTranslator/static/js/main.f36e3be5.chunk.js"
+  },
+  {
+    "revision": "ed64d6b1ffe20b9d44c0",
+    "url": "/LanguageTranslator/static/js/runtime-main.24aafd60.js"
+  }
+]);
